a0d2e082dc4371e180fbabcf574545dc348325b7
thayer11 <holly.coddington@gmail.com>
2016-08-19 10:51:49 -0600

    backend json

--- starter-code/public/scripts/app.js
+++ starter-code/public/scripts/app.js
@@ -1,1 +1,9 @@
-angular.module('CardsAgainstAssembly', []);
+angular.module('CardsAgainstAssembly', ['ngRoute']);
+
+function getAll(request, response) {
+  Card.find(function(error, CardsAgainstAssembly) {
+    if(error) response.json({message: 'Could not find a card'});
+
+    response.json({cards: cards});
+  }).select('-__v');
+}
--- starter-code/public/scripts/controllers/cardsController.js
+++ starter-code/public/scripts/controllers/cardsController.js
@@ -1,14 +1,57 @@
-angular.module('CardsAgainstAssembly')
+var app = angular.module('CardsAgainstAssembly')
   .controller('CardsController', CardsController);
 
-function CardsController(){
-  var vm = this;
-  vm.questionsList = [
-    {question: "What is Batman's guilty pleasure?"},
-    {question: "I'm sorry professor, I couldn't complete my homework because _________."},
-    {question: "I get by with a little help from _________."},
-    {question: "_________. It's a trap!"},
-    {question: "The class field trip was completely ruined by _________."},
-    {question: "What's my secret power?"}
-  ]
-}
+app.config(function($routeProvider, $locationProvider){
+  $routeProvider
+    .when('/', {
+      // template: 'Home!'
+      templateUrl: 'templates/card-index.html',
+      controller: 'CardsController'
+    })
+    .when('/cards/:id', {
+        templateUrl: 'templates/card-show.html',
+        controller: 'CardsController'
+    });
+
+    $locationProvider.html5Mode({
+      enabled: true,
+      requireBase: false
+    });
+})
+
+app.controller('CardsIndexCtrl',function($scope, $http){
+  this.$inject = ["$http"];
+    function getAllCards() {
+        $http.get('https://shielded-forest-41789.herokuapp.com/api/flashcards')
+            .then(function(response) {
+                $scope.cards = response.data;
+            });
+    }
+        getAllCards();
+});
+
+
+app.controller('CardsShowCtrl',function($scope, $http, $routeParams) {
+  console.log($routeParams.id);
+    this.$inject=["$http"];
+    function getCard(id) {
+        $http.get('https://shielded-forest-41789.herokuapp.com/api/flashcards'+id)
+            .then(function(response) {
+                $scope.card = response.data;
+            });
+    }
+    var cardID = $routeParams.id;
+    getCard(cardID);
+});
+
+// function CardsController(){
+//   var vm = this;
+//   vm.questionsList = [
+//     {question: "What is Batman's guilty pleasure?"},
+//     {question: "I'm sorry professor, I couldn't complete my homework because _________."},
+//     {question: "I get by with a little help from _________."},
+//     {question: "_________. It's a trap!"},
+//     {question: "The class field trip was completely ruined by _________."},
+//     {question: "What's my secret power?"}
+//   ]
+// }
